2cab3406f741a781d7b3ab6372fa637bf147c204
Sebastian <sebastian.tellez.o@gmail.com>
2015-06-21 12:46:10 -0500

    add model userprofiles

--- Buddis/app/src/main/java/com/chilangolabs/buddis/ProfileDetailActivity.java
+++ Buddis/app/src/main/java/com/chilangolabs/buddis/ProfileDetailActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.android.volley.Request;
@@ -12,10 +13,16 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.chilangolabs.buddis.Adapters.AdapterCard;
+import com.chilangolabs.buddis.Entitys.ItemCard;
 import com.chilangolabs.buddis.OwnElements.TextBuddis;
 import com.pkmmte.view.CircularImageView;
+import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class ProfileDetailActivity extends AppCompatActivity {
 
@@ -71,11 +78,29 @@ public class ProfileDetailActivity extends AppCompatActivity {
                 progressDialog.dismiss();
                 Log.e("response ", response.toString());
 
+                try {
+                    txtProfileName.setText(response.getString("displayName"));
+                    txtProfileExperience.setText(response.getJSONArray("talents").get(0).toString());
+                    Picasso.with(ProfileDetailActivity.this).load(response.getString("fbPicture")).placeholder(R.drawable.cargando).error(R.drawable.cargando).into(view);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                ArrayList<ItemCard> itemCards = new ArrayList<>();
+
+                itemCards.add(new ItemCard(R.drawable.cantoya, "Globos de cantoya reciclados"));
+                itemCards.add(new ItemCard(R.drawable.lampara, "Construcción de lamparas"));
+                AdapterCard adapter = new AdapterCard(ProfileDetailActivity.this, R.layout.item_card_job, itemCards);
+
+                listUserProfileJobs.setAdapter(adapter);
+
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 progressDialog.dismiss();
+                Toast.makeText(ProfileDetailActivity.this, "Algo salio mal, intenta de nuevo", Toast.LENGTH_SHORT).show();
+                finish();
             }
         });
 
